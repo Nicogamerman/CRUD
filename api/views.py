f7a402f8541b73be3407af1b8db116914907d4ff
nicogamerman <gamermanico@gmail.com>
2024-05-26 22:34:34 -0300

    first commit

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import PersonSerializer
+from .models import Person
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
